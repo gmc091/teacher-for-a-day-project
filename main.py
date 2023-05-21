0bc478bc9662f056a112817838e59eae80d01a16
Sylently <83719426+gmc091@users.noreply.github.com>
2023-05-21 14:31:19 +0200

    Update with basic search system, different adjustings and bug fixing.

--- main.py
+++ main.py
@@ -8,12 +8,14 @@ import speech_recognition as sr  # Used for speech recognition
 import language_tool_python  # Used for grammar and spelling correction (Not used in this example)
 import threading  # Used to run multiple threads
 import queue  # Used for thread-safe queue operations
+import webbrowser
 
 # Set the assistant's name
 ASSISTANT_NAME = "jarvis"
+is_talking = False
 
 # Read API key from file
-with open("api_key.txt", "r") as file:
+with open("openai_api_key.txt", "r") as file:
     api_key = file.read().strip()
 
 # Check if API key is set
@@ -31,32 +33,43 @@ is_assistant_active = False  # To check if the assistant is active
 conversation_history = [
     {
         "role": "system",
-        "content": "You are an helpful AI assistant made by Giovanni Croese for an english school project called 'Theacher for a day'",
+        "content": "You are an helpful AI assistant made by Giovanni Croese for an english school project called 'Theacher for a day' your default name is Jarvis, you love breaking bad, better call saul and star wars.",
     },
     {
         "role": "user",
-        "content": "Remember that when I say 'say my name', you should reply with 'You're Heisenberg' and when I say 'You're god damn right' tell me something about breaking bad, and if I say that I need a lawyer you have to tell me 'Better call saul!",
+        "content": "Remember that when I say 'Hello there' you have to answer with 'General Kenobi', when I say 'say my name', you should reply with 'You're Heisenberg' and when I say 'You're god damn right' tell me something about breaking bad, and if I say that I need a lawyer you have to tell me 'Better call saul!",
     },
     {
         "role": "assistant",
-        "content": "Of course, when you say 'say my name', I'll respond with 'You're Heisenberg' and when you'll say 'You're god damn right' I'll tell you something about breaking bad starting with the sentence 'I see you like breaking bad', and if you say that you need a lawyer I'm going to just tell you the only sentence: 'Better call saul!",
+        "content": "Of course, when you say 'Hello there' I will answer with 'Generalk Kenobi', when you say 'say my name', I'll respond with 'You're Heisenberg' and when you'll say 'You're god damn right' I'll tell you something about breaking bad starting with the sentence 'I see you like breaking bad', and if you say that you need a lawyer I'm going to just tell you the only sentence: 'Better call saul!",
     },
 ]
 
 
-# Initialize the text-to-speech engine
+# Initialize the text-to-\ engine
 engine = pyttsx3.init()
+engine.setProperty(
+    "voice",
+    "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_ZIRA_11.0",
+)
+
 speak_queue = queue.Queue()  # Create a queue to hold text to be spoken
+
+for voice in engine.getProperty("voices"):
+    print(voice)
 
 
 # Function to speak the text in the queue
 def speak_text():
+    global is_talking
     while True:
         text = speak_queue.get()  # Get text from queue
         if text is None:  # Break loop if text is None
             break
+        is_talking = True
         engine.say(text)  # Convert text to speech
         engine.runAndWait()  # Wait until speech is complete
+        is_talking = False
 
 
 # Start a new thread that will handle speaking text
@@ -136,6 +149,7 @@ def process_speech_input():
     global is_assistant_active
     global ASSISTANT_NAME
     global conversation_history
+    global is_talking
     recognizer = sr.Recognizer()  # Initialize the speech recognizer
     print("Started processing speech input")
 
@@ -177,6 +191,10 @@ def process_speech_input():
                 speak_queue.put(f"My name has been changed to {new_name}.")
                 continue
 
+            if "play a song" in text.lower():
+                webbrowser.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+                continue
+
             # If the assistant is active
             if is_assistant_active:
                 # Add the user's text to the conversation history
@@ -199,7 +217,8 @@ def process_speech_input():
                 speak_queue.put(reply)
 
         except sr.UnknownValueError:
-            print("Speech recognition could not understand audio")
+            if not is_talking:
+                print("Speech recognition could not understand audio")
         except sr.RequestError as e:
             print(f"Could not request results from speech recognition service; {e}")
         except Exception as e:
